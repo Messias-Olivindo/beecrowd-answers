f4e1d691c4261f1452876419978fa3be7ed8516c
Messias Olivindo <messias.olivindo@sou.inteli.edu.br>
2025-05-06 21:49:36 -0300

    add resolution of problem 1001 in javascript

---
+++ javascript/1001.js
@@ -0,0 +1,7 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var number = lines.map( str => Number(str));
+
+console.log("X = "+ (number[0] + number[1]));
+
